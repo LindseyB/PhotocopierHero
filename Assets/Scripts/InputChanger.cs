5b91aa2c10c10f8c8367eb2f7fb23afd4fd6dc37
Lindsey Bieda <lbieda@articulate.com>
2015-11-06 13:58:39 -0500

    Checking for input now works

--- Assets/Scripts/InputChanger.cs
+++ Assets/Scripts/InputChanger.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 public class InputChanger : MonoBehaviour {
 	private Sprite[] keySprites;
 	private const float DURATION = 3f;
 	private float waitTime = 0;
-	
+	private KeyCode key;
+
 	void Start() {
 		keySprites = Resources.LoadAll<Sprite>("Keys");
 	}
@@ -14,9 +17,15 @@ public class InputChanger : MonoBehaviour {
 	void Update() {
 		waitTime -= Time.deltaTime;
 
-		if (waitTime <= 0) {
+		if (waitTime <= 0 || Input.GetKeyDown(key)) {
 			waitTime = DURATION;
 			int index = Random.Range(0, keySprites.Length);
+
+			Regex keyRegex = new Regex(".*Keyboard_White_(.*).png");
+			Match match = keyRegex.Match(AssetDatabase.GetAssetPath(keySprites[index]));
+			Debug.Log(match.Groups[1].Value);
+			key = (KeyCode)System.Enum.Parse(typeof(KeyCode), match.Groups[1].Value);
+
 			gameObject.GetComponent<Image>().sprite = keySprites[index];
 
 			gameObject.GetComponent<RectTransform>().position = new Vector3 (Random.Range (50, 933),
